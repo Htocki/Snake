213c76bcb04f438ffc325eb49287fb2d56bd7d25
Htocki <pasztownia@gmail.com>
2024-05-31 22:23:56 +0000

    Код, ответственный за состояние, перенесен в отдельный класс.

--- Source/Snake/SnakeState.hpp
+++ Source/Snake/SnakeState.hpp
@@ -3,56 +3,44 @@
 #include "Input.hpp"
 #include "Snake.hpp"
 
-class MovingDownState;
-class MovingLeftState;
-class MovingRightState;
-class MovingUpState;
-class StandingState;
-
 class SnakeState {
 public:
-  static MovingDownState  movingDown;
-  static MovingLeftState  movingLeft;
-  static MovingRightState movingRight;
-  static MovingUpState    movingUp;
-  static StandingState    standing; 
-
-  virtual ~SnakeState() {} 
-  virtual void HandleInput(Snake& snake, Input input) = 0;
+  virtual ~SnakeState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) = 0;
   virtual void Update(Snake& snake) = 0;
 };
 
 class MovingDownState : public SnakeState {
 public:
-  MovingDownState();
-  virtual void HandleInput(Snake& snake, Input input) override;
+  MovingDownState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) override;
   virtual void Update(Snake& snake) override;
 };
 
 class MovingLeftState : public SnakeState {
 public:
-  MovingLeftState();
-  virtual void HandleInput(Snake& snake, Input input) override;
+  MovingLeftState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) override;
   virtual void Update(Snake& snake) override;
 };
 
 class MovingRightState : public SnakeState {
 public:
-  MovingRightState();
-  virtual void HandleInput(Snake& snake, Input input) override;
+  MovingRightState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) override;
   virtual void Update(Snake& snake) override;
 };
 
 class MovingUpState : public SnakeState {
 public:
-  MovingUpState();
-  virtual void HandleInput(Snake& snake, Input input) override;
+  MovingUpState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) override;
   virtual void Update(Snake& snake) override;
 };
 
 class StandingState : public SnakeState {
 public:
-  StandingState();
-  virtual void HandleInput(Snake& snake, Input input) override;
+  StandingState() {}
+  virtual SnakeState* HandleInput(Snake& snake, Input input) override;
   virtual void Update(Snake& snake) override;
 };
