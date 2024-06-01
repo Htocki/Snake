ed987abfd8f223a67eb4c2388bed62eb80699e41
Htocki <pasztownia@gmail.com>
2024-06-01 01:12:09 +0000

    Сохранение

--- Source/Snake/Snake.cpp
+++ Source/Snake/Snake.cpp
@@ -2,13 +2,47 @@
 
 #include "SnakeState.hpp"
 
-Snake::Snake()
+Snake::Snake(Position position)
   : m_state { new StandingState() }
   , m_isCollided { false }
-{}
+  , m_beginPosition { position }
+{
+  m_snakeSegments.push_back(position);
+}
 
 Snake::~Snake() {
   delete m_state;
+}
+
+void Snake::MoveDown() {
+  if (IsDownSideEmpty()) {
+    m_snakeSegments[0].y += 1;
+  }
+}
+
+void Snake::MoveLeft() {
+  if (IsLeftSideEmpty()) {
+    m_snakeSegments[0].x -= 1;
+  }
+}
+
+void Snake::MoveRight() {
+  if (IsRightSideEmpty()) {
+    m_snakeSegments[0].x += 1;
+  }
+}
+
+void Snake::MoveUp() {
+  if (IsUpSideEmpty()) {
+    m_snakeSegments[0].y -= 1;
+  }
+}
+
+void Snake::Reset() {
+  delete m_state;
+  m_state = new StandingState();
+  m_snakeSegments.clear();
+  m_snakeSegments.push_back(m_beginPosition);
 }
 
 bool Snake::IsCollided() const { return m_isCollided; }
@@ -25,9 +59,18 @@ void Snake::Update() {
 
 void Snake::Render(sf::RenderWindow& window) {}
 
-void Snake::CheckCollision() {}
+bool Snake::IsDownSideEmpty() const {
+  return true;
+}
 
-void Snake::Reset() {
-  delete m_state;
-  m_state = new StandingState();
+bool Snake::IsLeftSideEmpty() const {
+  return true;
+}
+
+bool Snake::IsRightSideEmpty() const {
+  return true;
+}
+
+bool Snake::IsUpSideEmpty() const {
+  return true;
 }
--- Source/Snake/Snake.hpp
+++ Source/Snake/Snake.hpp
@@ -1,27 +1,45 @@
 #pragma once
 
+#include <vector>
+
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
 
 #include "Input.hpp"
 
+using Position = sf::Vector2u;
+using SnakeSegments = std::vector<Position>;
+
 class SnakeState;
 
 class Snake {
 public:
-  Snake();
+  Snake(Position position);
   ~Snake();
 
+  Position GetPosition() { return m_snakeSegments[0]; }
+
+  void MoveDown();
+  void MoveLeft();
+  void MoveRight();
+  void MoveUp();
+  void Reset();
+
   bool IsCollided() const;
+  
   void HandleInput(Input input);
   void Update();
   void Render(sf::RenderWindow& window);
-  void Reset();
 
 private:
-  void CheckCollision();
+  bool IsDownSideEmpty() const;
+  bool IsLeftSideEmpty() const;
+  bool IsRightSideEmpty() const;
+  bool IsUpSideEmpty() const;
   
-  SnakeState* m_state;
-  bool        m_isCollided;
+  SnakeState*   m_state;
+  bool          m_isCollided;
+  SnakeSegments m_snakeSegments;
+  Position      m_beginPosition;
 };
